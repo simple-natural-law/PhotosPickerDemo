80070d02e8951b18c1acaa2b0a46c6370c84b431
zhangshijian <zsjcwy_xcode@163.com>
2017-10-25 22:10:05 +0800

    add

---
+++ PhotosPickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotosPickerDemo
+//
+//  Created by 张诗健 on 2017/10/25.
+//  Copyright © 2017年 张诗健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
